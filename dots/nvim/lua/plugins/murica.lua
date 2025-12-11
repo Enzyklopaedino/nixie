461f46d622ed167e99161feab797584631b456c9
Enzyklopaedino <dino2mail2@gmail.com>
2025-12-11 10:18:24 +0100

    feat: show hidden files on default

--- dots/nvim/lua/plugins/murica.lua
+++ dots/nvim/lua/plugins/murica.lua
@@ -7,6 +7,12 @@ return {
             "icon",
             "size",
         },
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                return name == ".."
+            end,
+        },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }
